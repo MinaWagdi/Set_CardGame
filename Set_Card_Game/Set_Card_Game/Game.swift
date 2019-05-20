438b79fa1b2c03f49b0cb697bb901a2cd9d59be9
MinaWagdi <MinaWagdi@users.noreply.github.com>
2019-05-20 10:14:20 +0200

    .

--- Set_Card_Game/Set_Card_Game/Game.swift
+++ Set_Card_Game/Set_Card_Game/Game.swift
@@ -13,10 +13,8 @@ struct Game{
     var cards = [Card]()
     var IndexToCardsArray:Int = 0 // to indicate to which card index we have shown in our view
     var set = Set()
-    static let shared = Game()
-    var score = 0
     
-    private init() {
+    init() {
         for shape in Shape.all(){
             for numberOfShapes in NumberOfShapes.all(){
                 for color in Color.all(){
@@ -30,8 +28,4 @@ struct Game{
         cards.shuffle()
         
     }
-//    func sad()-> Game{
-//        let g = Game()
-//        return g
-//    }
 }
--- Set_Card_Game/Set_Card_Game/ViewController.swift
+++ Set_Card_Game/Set_Card_Game/ViewController.swift
@@ -8,18 +8,14 @@
 
 import UIKit
 
-class Controller: UIViewController {
+class ViewController: UIViewController {
 
-    var game = Game.shared
+    var game = Game()
     var SelectedButtons : (button1: UIButton, button2: UIButton, button3: UIButton)? = nil
     var NumberOfSelectedButton : Int = 0
     var GameCardIndexVsButtonIndexDictionary = [Int:Int]() // the first Int is the index of the card of the game variable and the second Int is the index of the card in the Buttons outlet Collection array
     var attributes=[NSAttributedString.Key:Any]() //= [.font: 40.0]
     var emptyIndicesInCardsArrayOfButtons = [Int]()
-    var originator = Originator()
-    var caretaker = Caretaker()
-    
-    @IBOutlet weak var ScoreLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -89,10 +85,7 @@ class Controller: UIViewController {
         
         Cards[cardButtonIndexKey].setAttributedTitle(NSAttributedString(string:buttonShapeString,attributes: attributes), for: UIControl.State.normal)
         if(game.cards[cardIndex].isPartOfSet){
-            print("isPartOfSet")
             Cards[cardButtonIndexKey].backgroundColor = #colorLiteral(red: 0.2987604737, green: 0.2987604737, blue: 0.2987604737, alpha: 1)
-        }else if game.cards[cardIndex].isSelected==true{
-            SelectButton(sender: Cards[cardButtonIndexKey], gameCardIndex: cardIndex)
         }else{
             Cards[cardButtonIndexKey].backgroundColor=UIColor.white
         }
@@ -101,7 +94,6 @@ class Controller: UIViewController {
     
     
     func updateViewFromModel(){
-        NumberOfSelectedButton=0
 //        for card in Cards{
 //            card.isHidden=true
 //        }
@@ -119,15 +111,13 @@ class Controller: UIViewController {
     
     @IBAction func DrawCards(_ sender: UIButton) {
         if emptyIndicesInCardsArrayOfButtons.count>0{
-            print("DRAW CARDS")
             print("empty indices array is greater than 0")
             var sortedArray = emptyIndicesInCardsArrayOfButtons.sorted(by: <)
             print("sortedArray is \(sortedArray)")
             var cardsToDraw = 3
             for cardIndex in game.cards.indices{
                 if !game.cards[cardIndex].isVisible && !game.cards[cardIndex].isPartOfSet && GameCardIndexVsButtonIndexDictionary[cardIndex]==nil{
-                    game.cards[cardIndex].isVisible=true
-                    print("entered in the conditions at card \(cardIndex)")
+                    print("entered in the conditions")
                     GameCardIndexVsButtonIndexDictionary[cardIndex]=sortedArray[0]
                     showCardDetails(cardIndex: cardIndex, cardButtonIndexKey: sortedArray.remove(at: 0))
                     cardsToDraw -= 1
@@ -138,32 +128,14 @@ class Controller: UIViewController {
                 }
             }
         }else{
-            print("emptyindices array length is \(emptyIndicesInCardsArrayOfButtons.count)")
             return
         }
-    }
-    
-    @IBAction func clickUndo(_ sender: Any) {
-            let length = caretaker.mementoList.count-1
-            if length == -1{
-                return
-            }
-            print("length is \(length)")
-            originator.getStateFromMemento(memento: caretaker.mementoList.remove(at: length))
-            game.cards=originator.getCardsState()
-            game.set.setCards(cards: originator.getCardsOfSet())
-            print("score in originator is \(originator.getScore())")
-            print("score in game is \(game.score)")
-            if game.score < originator.getScore(){
-                game.score=originator.getScore()
-                updateScoreLabel()
-            }
-            updateViewFromModel()
+        
+        
     }
     
     @IBAction func touchCard(_ sender: UIButton) {
         if sender.backgroundColor != UIColor.clear{
-            saveState()
             let index = Cards.index(of:sender)
             // loop on all the cards available on the screen to search for the index of the card touched in the Model's game cards instance
             for gameCardIndex in GameCardIndexVsButtonIndexDictionary.keys{
@@ -172,23 +144,21 @@ class Controller: UIViewController {
                     print("Color is : \(game.cards[gameCardIndex].color)")
                     if game.cards[gameCardIndex].isSelected == false && NumberOfSelectedButton < 3{
                         SelectButton(sender: sender,gameCardIndex: gameCardIndex )
-                        game.set.addCardToSet(card: game.cards[gameCardIndex],index: gameCardIndex)
+                        NumberOfSelectedButton += 1
+                        game.set.addCard(card: game.cards[gameCardIndex],index: gameCardIndex)
                     }else if game.cards[gameCardIndex].isSelected == true{
                         diselectButton(sender: sender, gameCardIndex: gameCardIndex)
-                        game.set.removeCardFromSet(card: game.cards[gameCardIndex],index: gameCardIndex)
+                        NumberOfSelectedButton -= 1
+                        game.set.CancelCard(card: game.cards[gameCardIndex],index: gameCardIndex)
                     }else{
                         print("cannot be selected nor deselected, number of Selected Button is \(NumberOfSelectedButton)")
                         print("\(game.cards[gameCardIndex].isSelected)")
-                        return 
                     }
                     break
                 }
             }
             if NumberOfSelectedButton==3{
                 if game.set.isSet(){
-                    game.score += 2
-                    resetAllMemento()
-                    saveState()
                     print("Is Set")
                     let card1IndexInGameArray = game.set.card1.IndexOfCardInGameCardArray!
                     let card2IndexInGameArray = game.set.card2.IndexOfCardInGameCardArray!
@@ -203,64 +173,41 @@ class Controller: UIViewController {
                     game.cards[card3IndexInGameArray].isVisible=false
                     
                     if let CardButtonIndexReleased = GameCardIndexVsButtonIndexDictionary.removeValue(forKey: card1IndexInGameArray){
-//                        emptyIndicesInCardsArrayOfButtons.append(CardButtonIndexReleased)
-                        coloringButtonPartOfSet(sender:Cards[CardButtonIndexReleased],gameCardIndex:card1IndexInGameArray)
+                        emptyIndicesInCardsArrayOfButtons.append(CardButtonIndexReleased)
                     }else{
                         print("Nil value found when removing card1IndexInGameArray from GameCardIndexVsButtonIndexDictionary")
                     }
                     if let CardButtonIndexReleased = GameCardIndexVsButtonIndexDictionary.removeValue(forKey: card2IndexInGameArray){
-//                        emptyIndicesInCardsArrayOfButtons.append(CardButtonIndexReleased)
-                        coloringButtonPartOfSet(sender:Cards[CardButtonIndexReleased],gameCardIndex:card2IndexInGameArray)
+                        emptyIndicesInCardsArrayOfButtons.append(CardButtonIndexReleased)
                     }else{
                         print("Nil value found when removing card2IndexInGameArray from GameCardIndexVsButtonIndexDictionary")
                     }
                     if let CardButtonIndexReleased = GameCardIndexVsButtonIndexDictionary.removeValue(forKey: card3IndexInGameArray){
-//                        emptyIndicesInCardsArrayOfButtons.append(CardButtonIndexReleased)
-                        coloringButtonPartOfSet(sender:Cards[CardButtonIndexReleased],gameCardIndex:card3IndexInGameArray)
+                        emptyIndicesInCardsArrayOfButtons.append(CardButtonIndexReleased)
                     }else{
                         print("Nil value found when removing card3IndexInGameArray from GameCardIndexVsButtonIndexDictionary")
                     }
                     
-                    game.set.RemoveAllCardsFromSet()
+                    game.set.CancelAllCardsinSet()
+                    NumberOfSelectedButton=0
                     updateViewFromModel()
                 }
                 else{
-                    game.score -= 1
                     // TODO: deal with the mis matched cards
                 }
-                updateScoreLabel()
             }
         }
     }
-    func saveState(){
-        originator.setState(cards: game.cards, cardsOfSet:game.set.getAllCardsInSet(),score:game.score)
-        caretaker.add(memento: originator.saveStateToMemento())
-    }
-    func resetAllMemento(){
-        originator = Originator()
-        caretaker = Caretaker()
-        
-        print("Originator details \(originator.cardsOfSetState) \(originator.cardsState) \(originator.score)")
-        print("caretaker details \(caretaker.mementoList)")
-    }
-    func updateScoreLabel(){
-        ScoreLabel.text = "Score: \(game.score)"
-    }
 
-    func coloringButtonPartOfSet(sender:UIButton,gameCardIndex: Int){
-        sender.backgroundColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)
-    }
+    
+    
     func SelectButton(sender : UIButton, gameCardIndex: Int){
-        sender.backgroundColor = #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1)
+        sender.backgroundColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)
         game.cards[gameCardIndex].isSelected=true
-        NumberOfSelectedButton+=1
-        print("NUmber of Selected Buttons  is \(NumberOfSelectedButton)")
     }
     func diselectButton(sender: UIButton, gameCardIndex: Int){
         sender.backgroundColor = UIColor.white
         game.cards[gameCardIndex].isSelected = false
-        NumberOfSelectedButton-=1
-        print("NUmber of Selected Buttons  is \(NumberOfSelectedButton)")
     }
     
     
